1f3754b57eac0eae0d4d9a0b06d490d012b1b1e7
Sofia Kakarova <sofiakakarova01@gmail.com>
2024-02-28 08:29:35 +0200

    first commit

---
+++ laba1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        char symbol = 'C'; 
+        List<string> stringList = new List<string> { "Cat", "Dog", "Car", "CiviC", "Lion", "ABC","Computer","CliniC" }; 
+      
+        int count = stringList.Count(s =>
+            s.Length > 1 &&
+            s.First() == symbol &&
+            s.Last() == symbol);
+
+        Console.WriteLine($"Кількість елементів, які містять більше одного символу та починаються і закінчуються символом {symbol}: {count}");
+    }
+}
+
+
+
+        
+        
+
